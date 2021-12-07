dd139720dfff94998d8867466f77f0a65ab789c9
Alexandre GRANIE <Alexandre.GRANIE@mairie-avignon.com>
2021-12-07 14:45:03 +0100

    test script

--- leaflet-routing-machine.js
+++ leaflet-routing-machine.js
@@ -1,14 +1,1 @@
 // JavaScript source code
-var map = L.map('map');
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-}).addTo(map);
-
-L.Routing.control({
-    waypoints: [
-        L.latLng(57.74, 11.94),
-        L.latLng(57.6792, 11.949)
-    ],
-    routeWhileDragging: true
-}).addTo(map);
